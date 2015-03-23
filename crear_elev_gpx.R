e4fcd7bf7d62134580e38f9779351110b28ad677
Mateo Neira <mateo0719@gmail.com>
2015-03-23 10:57:28 -0500

    script mejorado
    agregado capacidad de leer multiples tracks y grabar cada una en un
    archivo distinto.

--- crear_elev_gpx.R
+++ crear_elev_gpx.R
@@ -29,40 +29,41 @@ leer<-function(directorio){
   id<-as.integer(length(files))
   id<-c(1:id)
   print(id)
-  name<-0
   for (i in id){
-    name<-name+1
-    print(name)
+    print(i)
     #leer archivo gpx
     raw<-readGPX(files[i])
-    #limpiar datos
-    data<-raw[4][[1]][[1]][[1]]
-    #agregar tiempo en formato tiempo
-    data["temp"]<-substr(data$time, 12, 19)
-    data["tiempo"]<-chron(times=data$temp)
-    #grabar minimo del valor tiempo
-    mintime<-min(data$tiempo)
-    #agregar tiempo reescalado segun minimo
-    data["tiempo_E"]<-as.character(data$tiempo-mintime)
-    #agregar tiempo en segundos
-    data["seg"]<-sapply(data$tiempo_E, segundos)
-    #cambiar formato tiempo
-    data["time"]<-sapply(data$time, crear_tiempo)
-    #crear elevacion a partir de segundos // factor de conversion puede cambiar
-    data["ele"]<-data$seg
-    #elimnar columnas innecesarias
-    data$temp<-NULL
-    data$tiempo<-NULL
-    data$tiempo_E<-NULL
-    data$seg<-NULL
-    
-    #escribir archivos
-    nombre1<-paste(i,"_elev",".gpx",sep="")
-    gpx_new<-SpatialPointsDataFrame(data=data,  coords=data[,1:2], proj4string=CRS("+proj=longlat +datum=WGS84"))
-    writeOGR(gpx_new, dsn=nombre1, layer="trackpoints", driver="GPX", dataset_options="GPX_USE_EXTENSIONS=yes")
-    
-    
-    
+    tracks<-as.integer(length(raw[4][[1]]))
+    tracks<-c(1:tracks)
+    for(e in tracks){
+      #limpiar datos
+      data<-raw[4][[1]][[e]][[1]]
+      #agregar tiempo en formato tiempo
+      data["temp"]<-substr(data$time, 12, 19)
+      data["tiempo"]<-chron(times=data$temp)
+      #grabar minimo del valor tiempo
+      mintime<-min(data$tiempo)
+      #agregar tiempo reescalado segun minimo
+      data["tiempo_E"]<-as.character(data$tiempo-mintime)
+      #agregar tiempo en segundos
+      data["seg"]<-sapply(data$tiempo_E, segundos)
+      #cambiar formato tiempo
+      data["time"]<-sapply(data$time, crear_tiempo)
+      #crear elevacion a partir de segundos // factor de conversion puede cambiar
+      data["ele"]<-data$seg
+      #elimnar columnas innecesarias
+      data$temp<-NULL
+      data$tiempo<-NULL
+      data$tiempo_E<-NULL
+      data$seg<-NULL
+      
+      
+      #escribir archivos
+      nombre1<-paste(i,"_",e,"_elev",".gpx",sep="")
+      print(nombre1)
+      gpx_new<-SpatialPointsDataFrame(data=data,  coords=data[,1:2], proj4string=CRS("+proj=longlat +datum=WGS84"))
+      writeOGR(gpx_new, dsn=nombre1, layer="tracks", driver="GPX", dataset_options="GPX_USE_EXTENSIONS=yes")
+    }      
   }
   print("acabado!")
 }
