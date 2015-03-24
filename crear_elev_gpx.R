14a25185fc17c31449ef6f08eb5c6dc555cf9bf2
Mateo Neira <mateo0719@gmail.com>
2015-03-24 11:41:44 -0500

    crear tracks en gpx
    script ahora crea tracks en vez de waypoints.
    falta reescalar tiempo por track id, actualmente reescala todo segun el
    minimo de todos los tracks

--- crear_elev_gpx.R
+++ crear_elev_gpx.R
@@ -1,9 +1,6 @@
 #cargar libreria de plotKML,rgdal,sp, permite leer archivos gpx y chron para trabajar con tiempos
-library(plotKML)
 library(chron)
 library(rgdal)
-library(sp)
-
 
 #funcion para convertir el tiempo a segundos
 segundos <- function(x)
@@ -32,38 +29,33 @@ leer<-function(directorio){
   for (i in id){
     print(i)
     #leer archivo gpx
-    raw<-readGPX(files[i])
-    tracks<-as.integer(length(raw[4][[1]]))
-    tracks<-c(1:tracks)
-    for(e in tracks){
-      #limpiar datos
-      data<-raw[4][[1]][[e]][[1]]
-      #agregar tiempo en formato tiempo
-      data["temp"]<-substr(data$time, 12, 19)
-      data["tiempo"]<-chron(times=data$temp)
-      #grabar minimo del valor tiempo
-      mintime<-min(data$tiempo)
-      #agregar tiempo reescalado segun minimo
-      data["tiempo_E"]<-as.character(data$tiempo-mintime)
-      #agregar tiempo en segundos
-      data["seg"]<-sapply(data$tiempo_E, segundos)
-      #cambiar formato tiempo
-      data["time"]<-sapply(data$time, crear_tiempo)
-      #crear elevacion a partir de segundos // factor de conversion puede cambiar
-      data["ele"]<-data$seg
-      #elimnar columnas innecesarias
-      data$temp<-NULL
-      data$tiempo<-NULL
-      data$tiempo_E<-NULL
-      data$seg<-NULL
-      
-      
-      #escribir archivos
-      nombre1<-paste(i,"_",e,"_elev",".gpx",sep="")
-      print(nombre1)
-      gpx_new<-SpatialPointsDataFrame(data=data,  coords=data[,1:2], proj4string=CRS("+proj=longlat +datum=WGS84"))
-      writeOGR(gpx_new, dsn=nombre1, layer="tracks", driver="GPX", dataset_options="GPX_USE_EXTENSIONS=yes")
-    }      
+    (layers <- ogrListLayers(files[i]))
+    data <- readOGR(files[i], layer=layers[5])
+    #agregar tiempo en formato tiempo
+    data$temp<-substr(data$time, 12, 19)
+    data$tiempo<-chron(times=data$temp)
+    #grabar minimo del valor tiempo
+    mintime<-min(data$tiempo)
+    #agregar tiempo reescalado segun minimo
+    data$tiempo_E<-as.character(data$tiempo-mintime)
+    #agregar tiempo en segundos
+    data$seg<-sapply(data$tiempo_E, segundos)
+    #cambiar formato tiempo
+    data$time<-paste("2015/02/02", data$tiempo_E, sep=" ")
+    #crear elevacion a partir de segundos // factor de conversion puede cambiar
+    data$ele<-data$seg
+    #elimnar columnas innecesarias
+    data$temp<-NULL
+    data$tiempo<-NULL
+    data$tiempo_E<-NULL
+    data$seg<-NULL
+    
+    
+    #escribir archivos
+    nombre1<-paste(i,"_elev",".gpx",sep="")
+    print(nombre1)
+    writeOGR(data, dsn=nombre1, layer="track_points", driver="GPX", dataset_options="GPX_USE_EXTENSIONS=yes")
   }
   print("acabado!")
 }
+
