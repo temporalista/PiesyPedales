dc98436b74bb83305d275e4b3e6f45ce640d8a67
temporalista <danielorellana@LLACTALAB3s-iMac.local>
2016-04-15 12:22:59 -0500

    gps process

--- gps_process.R
+++ gps_process.R
@@ -1,6 +1,10 @@
+##Processing GPX files
+##Input: gpx raw file
+##outputs
 
+##Work in progress
 
-packages <- c("XML","rgdal","lubridate", "raster", "sp")
+packages <- c("XML","rgdal","lubridate", "raster", "sp", "dplyr")
 
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
@@ -20,7 +24,8 @@ shift.vec <- function (vec, shift) {
 
 
 #leer archivo gps
-gpsfile <- "15101603.gpx"
+setwd("/Volumes/MacData/odrive/GDrive_UC/geo/Exp PyP/2015_12_22_Experimento 2G/P0021135423/")
+gpsfile <- "15101501.gpx"
 layers <- ogrListLayers(gpsfile)
 gpsdata <- readOGR(gpsfile, layer=layers[5])
 
@@ -37,11 +42,11 @@ lons <- as.numeric(gpsdata$coords.x1)
 lats <- as.numeric(gpsdata$coords.x2)
 
 gps_df <- data.frame(trackid =trackids, segmentid=segmentids, pointid=pointsids, lat = lats, lon = lons, ele = elevs, time = times)
-rm(list=c("trackids","segmentids","pointsids", "elevs", "lats", "lons", "gpsfile", "times", "gpsdata"))
+rm(list=c("trackids","segmentids","pointsids", "elevs", "lats", "lons", "times", "gpsdata"))
 
-# Shift vectors for lat and lon so that each row also contains the next position.
-gps_df$lat.p1 <- shift.vec(gps_df$lat, -1)
-gps_df$lon.p1 <- shift.vec(gps_df$lon, -1)
+# Shift vectors for lat and lon so that each row also contains the previous position.
+gps_df$lat.p1 <- shift.vec(gps_df$lat, 1)
+gps_df$lon.p1 <- shift.vec(gps_df$lon, 1)
 
 
 # Calculate distances (in metres) using the function pointDistance from the ‘raster’ package.
@@ -54,55 +59,78 @@ gps_df$dist <- apply(gps_df, 1, FUN = function (row) {
 })
 
 
-
-# Transform the column ‘time’ so that R knows how to interpret it.
-gps_df$time <- strptime(gps_df$time, format = "%Y/%m/%d %H:%M:%S")
-# Shift the time vector, too.
-gps_df$time.p1 <- shift.vec(gps_df$time, -1)
 # Calculate the number of seconds between two positions.
-gps_df$timestep <- as.numeric(difftime(gps_df$time.p1, gps_df$time))
+gps_df$timestep <- as.numeric(
+  difftime(
+    strptime(gps_df$time, format = "%Y/%m/%d %H:%M:%S"), 
+    strptime(lag(gps_df$time), format = "%Y/%m/%d %H:%M:%S")))
 
 
 # Calculate metres per seconds, kilometres per hour and two LOWESS smoothers to get rid of some noise.
-gps_df$speed.m.per.sec <- gps_df$dist / gps_df$timestep
-gps_df$speed.km.per.h <- gps_df$speed.m.per.sec * 3.6
-gps_df$speed.km.per.h <- ifelse(is.na(gps_df$speed.km.per.h), 0, gps_df$speed.km.per.h)
+gps_df$mps <- gps_df$dist / gps_df$timestep
+gps_df$kph <- gps_df$mps * 3.6
+gps_df$mps_low <- lowess(gps_df$mps, f = 0.01)$y
+#gps_df$kph <- ifelse(is.na(gps_df$kph), 0, gps_df$kph)
 
-gps_tr <- gps_df[gps_df$trackid==0,]
+sd(gps_df$mps, na.rm=T)
+mean(gps_df$mps, na.rm=T)
+summary(gps_df$kph)
 
-# Plot elevations and smoother
-gps_tr$lowess.ele <- lowess(gps_tr$ele, f = 0.1)$y
-plot(gps_tr$ele, type = "l", bty = "n", xaxt = "n",
-     ylab = "Elevation", xlab = "time",col = "grey40")
-lines(gps_tr$lowess.ele, col = "red", lwd = 2)
-legend(x="bottomright", legend = c("GPS elevation", "LOWESS elevation"), 
-       col = c("grey40", "red"), lwd = c(1,3), bty = "n")
+#distance based segmentation
+##Chequear
+registros <- c(1:nrow(gps_df))
+gps_df$seq <- registros
+segn=1
+
+for (i in registros){
+  
+  if (!is.na(gps_df[i,"kph"])){
+    if(gps_df[i,"kph"]>10){
+      segn <- segn + 1
+      seq = 1
+    }
+    seq <- seq + 1
+    gps_df[i,"seq"] <- seq
+    gps_df[i,"segmentid"] <- segn
+  }
+}
+  
 
 
-# Plot speed and smoother
-gps_tr$lowess.speed <- lowess(gps_tr$speed.km.per.h, f = 0.1)$y
-plot(gps_tr$speed.km.per.h, type = "l", bty = "n",
+
+# 
+# 
+# gps_tr <- gps_df[gps_df$trackid==0,]
+# 
+# # Plot elevations and smoother
+# gps_tr$lowess.ele <- lowess(gps_tr$ele, f = 0.1)$y
+# plot(gps_tr$ele, type = "l", bty = "n", xaxt = "n",
+#      ylab = "Elevation", xlab = "time",col = "grey40")
+# lines(gps_tr$lowess.ele, col = "red", lwd = 2)
+# legend(x="bottomright", legend = c("GPS elevation", "LOWESS elevation"), 
+#        col = c("grey40", "red"), lwd = c(1,3), bty = "n")
+# 
+# 
+# # Plot speed and smoother
+plot(gps_df$speed.km.per.h, type = "l", bty = "n",
      xaxt = "n", ylab = "Speed", xlab = "",col = "grey40")
 lines(gps_tr$lowess.speed, col = "blue", lwd = 3)
 legend(x="bottomright", legend = c("GPS speed", "LOWESS speed"), 
        col = c("grey40", "blue"), lwd = c(1,3), bty = "n")
+# 
+# plot(gps_tr$lowess.ele,type="o")
+# lines(gps_tr$lowess.speed, col="blue")
+# 
+# # Plot distances 
+# plot(gps_tr$dist, type = "l", bty = "n",
+#      xaxt = "n", ylab = "distance", xlab = "",col = "grey40")
+# legend(x="bottomright", legend = c("distance"), 
+#        col = c("grey40", "blue"), lwd = c(1,3), bty = "n")
 
-plot(gps_tr$lowess.ele,type="o")
-lines(gps_tr$lowess.speed, col="blue")
+write.csv(gps_df, file=paste(gpsfile,".csv", sep = ""))
 
-# Plot distances 
-plot(gps_tr$dist, type = "l", bty = "n",
-     xaxt = "n", ylab = "distance", xlab = "",col = "grey40")
-legend(x="bottomright", legend = c("distance"), 
-       col = c("grey40", "blue"), lwd = c(1,3), bty = "n")
+coordinates(gps_df) = ~lon+lat
+gps_df <-  as.data.frame(gps_df)
 
-gps_bk <- gps_tr
-
-hist(gps_tr$ele)
-
-gps_tr <- gps_tr[gps_tr$dist<400,]
-gps_tr <- gps_tr[gps_tr$ele>2000,]
-
-coordinates(gps_tr) = ~lon+lat
-spplot(gps_tr["dist"], type="l")
+spplot(gps_df["segmentid"])
 
